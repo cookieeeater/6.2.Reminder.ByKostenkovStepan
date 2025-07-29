c04199b8496e0c628b300283881b4e4d0f6a6ba0
cookieeeater <stepa-kostenkov@mail.ru>
2025-07-29 17:15:44 +0700

    Программа напоминаний в среде PyCharm

---
+++ 6.2.Reminder.ByKostenkovStepan.py
@@ -0,0 +1,65 @@
+from tkinter import *
+from tkinter import messagebox as mb
+from tkinter import simpledialog as sd
+import datetime
+import pygame
+import time
+
+t = 0
+music = False
+
+def set():
+    global t, rem_text
+    rem = sd.askstring("Время напоминания","Введите время в формате ЧЧ:ММ (24 - часовой формат)")
+    if rem:
+        try:
+            hour = int(rem.split(":")[0])
+            minute = int(rem.split(":")[1])
+            now = datetime.datetime.now()
+            print(now)
+            dt = now.replace(hour=hour, minute=minute, second=0)
+            print(dt)
+            t = dt.timestamp()
+            print(t)
+            rem_text = sd.askstring("Текст напоминания", "Введите текст напоминания:")
+            label.config(text=f"Напоминание установлено на: {hour: 02}: {minute: 02} с текстом {rem_text}")
+        except ValueError:
+            mb.showerror("Ошибка", "Неверный формат времени")
+
+def check():
+    global t
+    if t:
+        now = time.time()
+        if now >= t:
+            play_snd()
+            t = 0
+    window.after(10000, check)
+
+def play_snd():
+    global music
+    music = True
+    pygame.mixer.init()
+    pygame.mixer.music.load("6.2.Reminder.ByKostenkovStepan.mp3")
+    pygame.mixer.music.play()
+
+def stop_music():
+    global music
+    if music:
+        pygame.mixer.music.stop()
+        music = False
+    label.config(text="Установить новое напоминание")
+
+window = Tk()
+window.title("Напоминание")
+label = Label(text="Установите напоминание", font=("Arial", 14))
+label.pack(pady=10)
+
+set_button = Button(text="Установить напоминание", command=set)
+set_button.pack(pady=10)
+
+stop_button = Button(text="Остановить музыку", command=stop_music)
+stop_button.pack(pady=5)
+
+check()
+
+window.mainloop()
